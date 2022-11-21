26d652822d9678581a61fc77b9256ec6f6d0892e
Jasmineyangyangyang <49432431+Jasmineyangyangyang@users.noreply.github.com>
2022-11-21 19:51:29 +0800

    change line222:"3.5.0" to "3.12.0"

--- Util/BuildTools/Setup.sh
+++ Util/BuildTools/Setup.sh
@@ -219,7 +219,7 @@ else
 
   # rpclib does not use any cmake 3.9 feature.
   # As cmake 3.9 is not standard in Ubuntu 16.04, change cmake version to 3.5
-  sed -i s/"3.9.0"/"3.5.0"/g ${RPCLIB_BASENAME}-source/CMakeLists.txt
+  sed -i s/"3.9.0"/"3.12.0"/g ${RPCLIB_BASENAME}-source/CMakeLists.txt
 
   mkdir -p ${RPCLIB_BASENAME}-libcxx-build
 
